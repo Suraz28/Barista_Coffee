18d7bea623b640b2d9f244a4495708b0d35ba238
Suraz Subedi <surazsubedi28@gmail.com>
2024-06-18 08:07:17 +0545

    updateed the urls

--- server/index.js
+++ server/index.js
@@ -56,8 +56,8 @@ app.post("/api/create-checkout-session", async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: "http://localhost:5173/success",
-      cancel_url: "http://localhost:5173/cancel",
+      success_url: `${process.env.FRONTEND_URL}/success`,
+      cancel_url: `${process.env.FRONTEND_URL}/cancel`,
     });
 
     res.json({ id: session.id });
