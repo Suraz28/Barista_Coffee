1681b8be60ccf7502f78e5a7e015d4e93854b15a
Suraz Subedi <surazsubedi28@gmail.com>
2024-06-18 08:46:56 +0545

    update

--- server/index.js
+++ server/index.js
@@ -52,8 +52,8 @@ app.post("/api/create-checkout-session", async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: "https://barista-coffee.vercel.app/success",
-      cancel_url: "https://barista-coffee.vercel.app/cancel",
+      success_url: "http://localhost:5173/success",
+      cancel_url: "http://localhost:5173/cancel",
     });
 
     res.json({ id: session.id });
